daab91f7fe9485099adadc7868996cc9cf9a5ce8
Patrick <patrick.x.rivera@gmail.com>
2018-03-13 23:45:20 -0400

    Fixed mixed content bug

--- src/redux/helpers/helpers.js
+++ src/redux/helpers/helpers.js
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone';
 
 export const API_KEY = '5367d13ed05e34e3342d4fdbc4f5cb7e';
-export const ROOT_URL = `http://api.openweathermap.org/data/2.5/`;
+export const ROOT_URL = `https://api.openweathermap.org/data/2.5/`;
 
 export const round = (num) => Math.round(num);
 
